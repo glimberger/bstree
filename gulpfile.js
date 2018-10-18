528e2e67798367df4d37bbbac2abd317069d01a6
Guillaume Limberger <guillaume.limberger@lecnam.net>
2018-10-18 21:06:27 +0200

    Update deps and au revoir bower.
    Replace deprecated gulp-utils dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    cleanCSS = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    browserSync = require('browser-sync').create(),
-    runSequence = require('run-sequence');
+  c = require('ansi-colors'),
+  log = require('fancy-log'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  cleanCSS = require('gulp-clean-css'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create()
 
 gulp.task('sass', function () {
-    gulp.src('src/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({sourceComments: 'map'}).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/css'));
-});
+  gulp.src('src/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({sourceComments: 'map'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('src/css'))
+})
 
 gulp.task('sass:dist', function () {
-    gulp.src('src/scss/bstree.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(cleanCSS({debug: true}, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
-});
+  gulp.src('src/scss/bstree.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cleanCSS({debug: true}, function (details) {
+      console.log(details.name + ': ' + details.stats.originalSize)
+      console.log(details.name + ': ' + details.stats.minifiedSize)
+    }))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
+})
 
 gulp.task('js:dist', function () {
-    return gulp.src('src/js/jquery.bstree.js')
-        .pipe(gulp.dest('dist/js'))
-        .pipe(uglify())
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest('dist/js'));
-});
+  return gulp.src('src/js/jquery.bstree.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('dist/js'))
+})
 
-gulp.task('watch', function() {
-    browserSync.init({
-        server: "./"
-    });
-    gulp.watch('src/scss/*.scss', ['sass']).on('change', function(event) {
-            gutil.log('File', gutil.colors.cyan(event.path), 'was', gutil.colors.yellow(event.type), ', running tasks...');
-        });
-    gulp.watch('src/css/*.css').on('change', browserSync.reload);
-    gulp.watch('src/js/*.js').on('change', browserSync.reload);
-    gulp.watch('demo/**/*.html').on('change', browserSync.reload);
-});
+gulp.task('watch', function () {
+  browserSync.init({
+    server: './'
+  })
+  gulp.watch('src/scss/*.scss', ['sass']).on('change', function (event) {
+    log('File', c.cyan(event.path), 'was', c.yellow(event.type), ', running tasks...')
+  })
+  gulp.watch('src/css/*.css').on('change', browserSync.reload)
+  gulp.watch('src/js/*.js').on('change', browserSync.reload)
+  gulp.watch('demo/**/*.html').on('change', browserSync.reload)
+})
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass'])
 
-gulp.task('dist', ['sass:dist', 'js:dist']);
+gulp.task('dist', ['sass:dist', 'js:dist'])
